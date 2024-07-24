a4d02e19c1d70411e7e2c1bda4fe8fa4b53d7dde
Diarmuid McGonagle <diarmuidmcgonagle@gmail.com>
2024-07-24 13:31:58 +0200

    Initial Commit

---
+++ MealzOnboardingSupplierSwiftUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MealzOnboardingSupplierSwiftUI
+//
+//  Created by miam x didi on 24/07/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MealzOnboardingSupplierSwiftUI/MealzOnboardingSupplierSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MealzOnboardingSupplierSwiftUIApp.swift
+//  MealzOnboardingSupplierSwiftUI
+//
+//  Created by miam x didi on 24/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct MealzOnboardingSupplierSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
